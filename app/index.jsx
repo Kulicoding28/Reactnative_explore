c54b47d2d8b30d930d9c2a23abcd694834ceee54
zerodev <ahmadabdulaziz380@gmail.com>
2024-04-15 18:58:30 +0700

    setup_nativewind

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,11 @@
 import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Gibahtech</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-3xl">Gibahtech</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{ color: "blue" }}>
         Go to profile
@@ -13,12 +13,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
